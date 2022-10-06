f37afce2132538ec8934b85a1aa71491443b34c7
wo6zy <102326511+wEquals@users.noreply.github.com>
2022-10-06 15:13:41 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,35 @@
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/1027575904609783899/90sUw7G2OVmc26sKbXbjJVvpApk3LdiqL-koGZVp4S5yBdsXos97TxhnAsT8bxGts-1s"
+local data = {
+   ["content"] = " ",
+   ["embeds"] = {
+       {
+           ["title"] = "**Someone Executed Solaris hub!**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
 
 if game.PlaceId == 4888256398 then
 
