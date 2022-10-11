984a66dfbe23c1585166e719d7e809d55762e35f
wo6zy <102326511+wEquals@users.noreply.github.com>
2022-10-11 18:27:06 +0100

    Update Synapse Version.lua

--- Synapse Version.lua
+++ Synapse Version.lua
@@ -288,17 +288,6 @@ Tab:AddButton({
   	end    
 })
 
-Tab:AddButton({
-	Name = "Anti-AFK",
-	Callback = function()
-		local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-  	end    
-})
 
 Tab:AddButton({
 	Name = "Reset Aimbot Settings",
