d0b2f697168266c26ff7fb83189b3d30470aaf6a
wo6zy <102326511+wEquals@users.noreply.github.com>
2022-12-12 19:36:09 +0000

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -276,7 +276,7 @@ function espLibrary.addEsp(player)
         }),
         box = create("Square", {
             Thickness = 1,
-            Filled = false
+            Filled = false,
             Size = 100, 100
         }),
         healthBarOutline = create("Square", {
